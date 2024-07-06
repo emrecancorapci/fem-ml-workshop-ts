56d50c83f0a3d154c9c45af38efdb2d790611709
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-07-06 21:46:00 +0300

    Update get-data to update tenserflow package from tfjs-node-gpu to tfjs-node and refactor

--- exercises/project-3/src/get-data.ts
+++ exercises/project-3/src/get-data.ts
@@ -1,52 +1,58 @@
-import * as tf from '@tensorflow/tfjs-node-gpu';
+import tf from '@tensorflow/tfjs-node';
 import fs from 'node:fs';
 import path from 'node:path';
+
+import {
+  IMAGE_SIZE,
+} from './config';
+
+const __dirname = import.meta.dirname;
 
 const trainImagesDir = path.join(__dirname, '../data/train');
 const testImagesDir = path.join(__dirname, '../data/test');
 
-function loadImages(dir: string) {
-  const imageAndLabels = fs.readdirSync(dir).map(function (file) {
-    const filePath = path.join(dir, file);
-    const buffer = fs.readFileSync(filePath);
-
-    const imageTensors = tf.node.decodeImage(buffer).resizeNearestNeighbor([20, 20]).expandDims();
-    
-    const isSquare = file.toLocaleLowerCase().endsWith('square.png');
-    const isTriangle = file.toLocaleLowerCase().endsWith('triangle.png');
-
-    const labels = isSquare ? 0 : isTriangle ? 1 : -1;
-
-    return { imageTensors, labels };
-  });
-
-  return imageAndLabels;
-  
-}
-
-export const loadTrainData = () => loadImages(trainImagesDir);
-export const loadTestData = () => loadImages(testImagesDir);
 
 export function getTrainData() {
-  const data = loadTrainData();
+  const data = loadImages(trainImagesDir)
 
   const imageSensors = data.map(({ imageTensors }) => imageTensors);
   const labels = data.map(({ labels }) => labels);
 
   return {
     images: tf.concat(imageSensors),
-    labels: tf.oneHot(tf.tensor1d(labels, 'float32'), 0)
-  }
+    labels: tf.oneHot(tf.tensor1d(labels, 'int32'), 2),
+  };
 }
 
 export function getTestData() {
-  const data = loadTestData();
+  const data = loadImages(testImagesDir);
 
   const imageSensors = data.map(({ imageTensors }) => imageTensors);
   const labels = data.map(({ labels }) => labels);
 
   return {
     images: tf.concat(imageSensors),
-    labels: tf.oneHot(tf.tensor1d(labels, 'float32'), 0)
-  }
+    labels: tf.oneHot(tf.tensor1d(labels, 'int32'), 2),
+  };
+}
+
+function loadImages(dir: string) {
+  return fs.readdirSync(dir).map((file) => generateTensorAndLabel(dir, file));
+}
+
+function generateTensorAndLabel (dir: string, file: string) {
+  const filePath = path.join(dir, file);
+  const buffer = fs.readFileSync(filePath);
+
+  const imageTensors = tf.node
+    .decodeImage(buffer)
+    .resizeNearestNeighbor([IMAGE_SIZE, IMAGE_SIZE])
+    .expandDims();
+
+  const isSquare = file.toLocaleLowerCase().endsWith('square.png');
+  const isTriangle = file.toLocaleLowerCase().endsWith('triangle.png');
+
+  const labels = isSquare ? 0 : isTriangle ? 1 : -1;
+
+  return { imageTensors, labels };
 }
